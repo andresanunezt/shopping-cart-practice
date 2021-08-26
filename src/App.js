9b8b7adb10994e9ebc205934eb41a3ec3f0e470f
Andrés Nuñez <andresanunez98@gmail.com>
2021-08-26 16:45:30 -0400

    added home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Navbar from "./components/NavBar";
-import { BrowserRouter } from "react-router-dom";
-import logo from "./logo.svg";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {/* <Route path="/cart" component={Cart}/> */}
+        </Switch>
       </div>
     </BrowserRouter>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,27 @@ import { connect } from "react-redux";
 
 class Home extends Component {
   render() {
+    let itemList = this.props.items.map((item) => {
+      return (
+        <div className="card" key={item.id}>
+          <div className="card-image">
+            <img src={item.img} alt={item.title} />
+            <span className="card-title">{item.title}</span>
+          </div>
+
+          <div className="card-content">
+            <p>{item.desc}</p>
+            <p>
+              <b>Price: {item.price}$</b>
+            </p>
+          </div>
+        </div>
+      );
+    });
     return (
       <div className="container">
-        <h3>Home</h3>
+        <h3 className="center">Our items</h3>
+        <div className="box">{itemList}</div>
       </div>
     );
   }
