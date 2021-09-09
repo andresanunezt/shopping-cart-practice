93ea2471ad46f8d43ed7b04b35c83dfb33559fd7
Andrés Nuñez <andresanunez98@gmail.com>
2021-09-09 14:31:22 -0400

    added styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer class="page-footer">
-      <div class="footer-copyright">
-        <div class="container">© 2021 Copyright Text</div>
-      </div>
+      <div class="container">© 2021 Copyright Text</div>
     </footer>
   );
 };
